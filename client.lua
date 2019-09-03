c53672dbb8ee67c9fcfa54d75ada94fe91022396
Darren <vampuriccode@gmail.com>
2019-09-03 17:26:11 -0400

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,6 +1,7 @@
 -- Created by Vampire - VampuricGaming.net
 Citizen.CreateThread(function()
-    local tDelay = 60000
+    local dirtLevel = 0.2
+    local tDelay = 20000
     local cleanModels = {
         [GetHashKey('police')] = true,
         [GetHashKey('police2')] = true,
@@ -10,9 +11,9 @@ Citizen.CreateThread(function()
         if IsPedInAnyVehicle(ped) then
             local veh = GetVehiclePedIsIn(ped, false)
             if cleanModels[GetEntityModel(veh)] ~= nil and cleanModels[GetEntityModel(veh)] then
-                WashDecalsFromVehicle(veh, 1.0)
-        		SetVehicleDirtLevel(veh, 0.0)
-                RemoveDecalsFromVehicle(veh)
+            	WashDecalsFromVehicle(veh, 1.0)
+        		SetVehicleDirtLevel(veh, dirtLevel)
+            	RemoveDecalsFromVehicle(veh)
             end
         end
 		Citizen.Wait(tDelay)
